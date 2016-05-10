778dc3094ee96749c1aeae1cc5b8e6874bc3c6ad
robotanarchy <robotanarchy@bingo-ev.de>
2016-05-10 21:33:07 +0200

    support adding multiple albums at once

--- random_album/random_album.sh
+++ random_album/random_album.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Adds exactly one random album from your music collection.
-# This works, as long as your collection follows a consistent scheme like
-# collection/artist/album. If it does not, consider using beets or a similar
-# tool to sort it.
+# Adds one or more ($1) random album(s) from your music collection.
+# This works, as long as your collection follows a consistent scheme
+# like collection/artist/album. If it does not, consider using beets or
+# a similar tool to sort it.
 # License: Public Domain
 # Part of: https://github.com/robotanarchy/robot_mpd_scripts
 #
@@ -10,9 +10,18 @@
 LIBRARY=/mnt/data/music/
 SCHEME='./collection/*/*'
 
+TIMES=1
+[ -z "$1" ] || TIMES=$1
+
+echo "Adding $TIMES random album(s)..."
+
 cd "$LIBRARY"
-find . -type d -wholename "${SCHEME}" \
-	| sort -R \
-	| head -n 1 \
-	| cut -d '/' -f 2- \
-	| mpc add
+for i in `seq 1 $TIMES`; do
+	find . -type d -wholename "${SCHEME}" \
+		| sort -R \
+		| head -n 1 \
+		| cut -d '/' -f 2- \
+		| mpc add
+done
+
+echo "Done!"
